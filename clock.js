9da66abcf61d72a8899815f115c7a76bae84f2b1
Peter <peterju35@gmail.com>
2015-02-02 03:44:07 -0500

    Comments

--- clock.js
+++ clock.js
@@ -1,9 +1,8 @@
-//lets display the current time
 var color;
 
 function displayTime() {
 	color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-	//color = "#"+"ff"+s+s;
+
 	//set background color
 	document.body.style.background = color;
 	//set time
