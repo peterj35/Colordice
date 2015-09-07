c385309f885889703473db8e3d8e2aa5004220fe
Peter <peterju35@gmail.com>
2015-09-07 11:49:50 -0400

    removed portf

---
+++ clip.js
@@ -0,0 +1,23 @@
+function SelectText(element) {
+    var doc = document
+        , text = doc.getElementById(element)
+        , range, selection
+    ;    
+    if (doc.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();        
+        range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
+
+document.onclick = function(e) {    
+    if (e.target.className === 'click') {
+        SelectText('hex');
+    }
+};
